192ca78dad7f8761e8128ef8177d1bc33ed337bc
EL FADILI MOHAMED <580.simo@gmail.com>
2024-08-25 23:24:37 +0100

    adding routing , navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,21 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {BrandsComponent} from "./brands/brands.component";
+import {CartComponent} from "./cart/cart.component";
+import {CategoriesComponent} from "./categories/categories.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {NotfoundComponent} from "./notfound/notfound.component";
 
 export const routes: Routes = [
-
+  {path:"",redirectTo:"home",pathMatch:"full"},
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"brands",component:BrandsComponent},
+  {path:"cart",component:CartComponent},
+  {path:"categories",component:CategoriesComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"**",component:NotfoundComponent},
 ];
