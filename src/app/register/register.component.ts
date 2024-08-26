03c65cf76003652d3be2481d583b84b3d3ade5cf
EL FADILI MOHAMED <580.simo@gmail.com>
2024-08-26 01:53:50 +0100

    adding register component with validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {JsonPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe,
+    NgIf
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm : FormGroup = new FormGroup({ // take an object of controls
+    // we declare the inputs give them the same names as server side
+    name : new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+    email: new FormControl(null,[Validators.required,Validators.email]),
+    password: new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/)]),
+    rePassword: new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/)]),
+    phone: new FormControl(null,[Validators.required]),
+  });
 
+  handleRegister(registerForm: FormGroup){
+    console.log(registerForm)
+
+  }
 }
+
+
