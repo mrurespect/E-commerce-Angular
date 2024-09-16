3cc6113ba0a1b79158c71dc896526af20d55412b
EL FADILI MOHAMED <580.simo@gmail.com>
2024-09-16 01:24:31 +0100

    dashboard main page

--- src/app/dashboard-main-page/dashboard-main-page.component.ts
+++ src/app/dashboard-main-page/dashboard-main-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-main-page',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './dashboard-main-page.component.html',
   styleUrl: './dashboard-main-page.component.css'
 })
